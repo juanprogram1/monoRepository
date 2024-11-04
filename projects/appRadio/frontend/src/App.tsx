e649ecb638e8bc89f611c14e4c09415bbd990879
Juan David Carrillo Anillo <anilloj62@gmail.com>
2024-11-04 15:10:23 -0500

    Modification

--- projects/appRadio/frontend/src/App.tsx
+++ projects/appRadio/frontend/src/App.tsx
@@ -1,35 +1,61 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./css/App.css";
+import { useEffect, useState, useRef } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const userName = "Juan";
+  const [message, setMessage] = useState<string[]>([]);
+  const [isConnected, setIsConnected] = useState(false);
+  const socketRef = useRef<WebSocket | null>(null);
+
+  useEffect(() => {
+    socketRef.current = new WebSocket("ws://localhost:3000");
+
+    socketRef.current.onopen = () => {
+      setIsConnected(true);
+      console.log("WebSocket is open");
+    };
+
+    socketRef.current.onmessage = (event) => {
+      const serverMessage = event.data;
+      setMessage((prevMessage) => [...prevMessage, serverMessage]);
+    };
+
+    socketRef.current.onclose = () => {
+      setIsConnected(false);
+      console.log("WebSocket is closed");
+    };
+
+    return () => {
+      if (socketRef.current) {
+        socketRef.current.close();
+      }
+    };
+  }, []);
+
+  const sendMessage = () => {
+    if (socketRef.current && socketRef.current.readyState === WebSocket.OPEN) {
+      socketRef.current.send("Hello from the client");
+    } else {
+      console.log("WebSocket is not open");
+    }
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+    <div className="App">
+      <h1>Welcome to the radio {userName}</h1>
+      <button onClick={sendMessage} disabled={!isConnected}>
+        Send message
+      </button>
+      <ul>
+        {message.map((msg, index) => (
+          <li key={index}>{msg}</li>
+        ))}
+      </ul>
+      <p>
+        {isConnected ? "Conectado al servidor" : "Desconectado del servidor"}
       </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
